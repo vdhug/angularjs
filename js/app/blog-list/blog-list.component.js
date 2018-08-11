76e0168163fc315a30274d0d9c324096a6cc330c
vdhug <vitorhug97@gmail.com>
2018-08-11 14:04:55 -0300

    Adding content to render

--- js/app/blog-list/blog-list.component.js
+++ js/app/blog-list/blog-list.component.js
@@ -4,6 +4,14 @@ angular.module('blogList').
 		//template: '<div class=""><h1 class="new-class">{{title}}</h1><button ng-click="clickTest()">Click me</button></div>',
 		templateUrl: '/templates/blog-list.html',
 		controller: function($scope){
+
+			var blogItems = [
+				{title: "Title 1", id: 1, description: "First Item"},
+				{title: "Title 2", id: 2, description: "Second Item"},
+				{title: "Title 3", id: 3, description: "Third Item"},
+				{title: "Title 4", id: 4, description: "Fourth Item"}
+			]
+
 			$scope.title = "Hi there"
 			$scope.clicks = 0
 			$scope.clickTest = function(){
