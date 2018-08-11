41945f4ba93517bc5c8a1c1e6090e36e7eca2ab9
vdhug <vitorhug97@gmail.com>
2018-08-11 14:00:46 -0300

    Using external templates with angular

--- js/app/blog-list/blog-list.component.js
+++ js/app/blog-list/blog-list.component.js
@@ -1,14 +1,16 @@
 'use strict';
-angular.module('blogList')
-	.controller('BlogListController', function($scope){
-		console.log("Hello!")
-		$scope.title = "Hello There"
-		$scope.clicks = 0
-		$scope.clickTest = function(){
-			console.log("Clicked")
-		$scope.clicks++
-
-		$scope.title = "Clicked "+$scope.clicks+" times"
-
+angular.module('blogList').
+	component("blogList", {
+		//template: '<div class=""><h1 class="new-class">{{title}}</h1><button ng-click="clickTest()">Click me</button></div>',
+		templateUrl: '/templates/blog-list.html',
+		controller: function($scope){
+			$scope.title = "Hi there"
+			$scope.clicks = 0
+			$scope.clickTest = function(){
+				console.log("Clicked")
+				$scope.clicks++
+				$scope.title = "Clicked "+ $scope.clicks +" times"
+			}
 		}
 	});
+	
