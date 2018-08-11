c5a7e5e6c42716b51c0145f9a646ab35343633e7
vdhug <vitorhug97@gmail.com>
2018-08-11 14:14:55 -0300

    Iterate over blog posting in a template file using angular

--- js/app/blog-list/blog-list.component.js
+++ js/app/blog-list/blog-list.component.js
@@ -9,8 +9,10 @@ angular.module('blogList').
 				{title: "Title 1", id: 1, description: "First Item"},
 				{title: "Title 2", id: 2, description: "Second Item"},
 				{title: "Title 3", id: 3, description: "Third Item"},
-				{title: "Title 4", id: 4, description: "Fourth Item"}
+				{title: "Title 4", id: 4, description: "Fourth Item"},
+				{title: "Title 5", id: 4, description: "Fifth Item"}
 			]
+			$scope.items = blogItems
 
 			$scope.title = "Hi there"
 			$scope.clicks = 0
